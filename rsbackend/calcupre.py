649cdad99bf3abca47c61fb013c72ce9a8510941
JKiriS <jkzgyb@gmail.com>
2015-03-15 13:13:55 +0800

    cal upre

--- rsbackend/calcupre.py
+++ rsbackend/calcupre.py
@@ -4,18 +4,17 @@ import pymongo
 from bson import ObjectId
 import datetime
 import math
-import random
 import jieba
-from gensim import corpora, models, similarities
-from collections import defaultdict
 
-stopwords = {}.fromkeys([ line.rstrip().decode('utf-8') for line in open('stopwords.txt') ])
+stopwords = {}.fromkeys([ line.rstrip().decode('utf-8') \
+	for line in open('stopwords.txt') ])
 stopwords[' '] = 1
 
 def userPre(uid):
-	pre = {'_id':ObjectId(uid),'source':{},'category':{},'wd':{},'visited':[],'favorite':[],'count':1}
+	pre = {'_id':ObjectId(uid),'source':{},'category':{},'wd':{}}
 	pre['timestamp'] = datetime.datetime.now()
 	# get the latesttime 
+
 	latest = db.behavior.find({'uid':ObjectId(uid)})\
 		.sort('timestamp', pymongo.DESCENDING).limit(1)
 	if latest.count() > 0:
@@ -24,47 +23,51 @@ def userPre(uid):
 		db.upre.save(pre)
 		return pre
 	# deal with search behavior
-	behaviorlist = db.behavior.find({'uid':ObjectId(uid), 'action':'search'})\
-		.sort('timestamp', pymongo.DESCENDING).limit(1000)
-	for i in behaviorlist:
+	for i in db.behavior.find({'uid':ObjectId(uid), 'action':'search'})\
+		.sort('timestamp', pymongo.DESCENDING).limit(500):
 		# calculate timefactor 1 / (1 + exp( deltaT - 10))
 		deltaT = latesttime - i['timestamp']
-		timefactor = 1 / (1 + math.exp((deltaT.total_seconds()/24/3600.) - 60.))
+		timefactor = 1 / (1 + math.exp((deltaT.total_seconds()/24/3600.) - 30.))
 		if i['ttype'] == 'source':
 			pre['source'][i['target']] = pre['source'].get(i['target'], 0) + 1 * timefactor
 		elif i['ttype'] == 'category':
 			pre['category'][i['target']] = pre['category'].get(i['target'], 0) + 1 * timefactor
 		elif i['ttype'] == 'wd':
-			pre['wd'][i['target']] = pre['wd'].get(i['target'], 0) + 1 * timefactor
+			segs = filter(lambda s:s not in stopwords, jieba.cut(i['target'], cut_all=False))
+			for s in segs:
+				pre['wd'][s] = pre['wd'].get(s, 0) + 1 * timefactor
 	
+	favos = {}
 	for i in db.behavior.find({'uid':ObjectId(uid), 'action':'addfavorite', 'ttype':'item'})\
 			.sort('timestamp', pymongo.DESCENDING).limit(200):
-		pre['favorite'].append([ObjectId(i['target']), i['timestamp']])
-	
+		favos[ObjectId(i['target'])] = i['timestamp']
+	visits = {}
 	for i in db.behavior.find({'uid':ObjectId(uid), 'action':'click', 'ttype':'item'})\
 			.sort('timestamp', pymongo.DESCENDING).limit(300):
-		pre['visited'].append([ObjectId(i['target']), i['timestamp']])
+		visits[ObjectId(i['target'])] = i['timestamp']
 
-	items = {}
-	for i, t in pre['visited']:
-		items[i] = t
-	latest = max(items.values())
-	for i in db.item.find({'_id':{'$in':items.keys()}}):
-		deltaT = latest - items[i['_id']]
-		timefactor = 1 / (1 + math.exp((deltaT.total_seconds()/24/3600.) - 60.))
-		pre['source'][i['source']] = pre['source'].get(i['source'], 0) + 1.2 * timefactor
-		pre['category'][i['category']] = pre['category'].get(i['category'], 0) + 1.2 * timefactor
+	if len(visits) > 0:
+		latest = max(visits.values())
+		for i in db.item.find({'_id':{'$in':visits.keys()}}):
+			deltaT = latest - visits[i['_id']]
+			timefactor = 1 / (1 + math.exp((deltaT.total_seconds()/24/3600.) - 30.))
+			pre['source'][i['source']] = pre['source'].get(i['source'], 0) + 1.2 * timefactor
+			pre['category'][i['category']] = pre['category'].get(i['category'], 0) + 1.2 * timefactor
+			segs = filter(lambda s:s not in stopwords, jieba.cut(i['title'], cut_all=False))
+			for s in segs:
+				pre['wd'][s] = pre['wd'].get(s, 0) + 1 * timefactor
 
-	items = {}
-	for i, t in pre['favorite']:
-		items[i] = t
-	latest = max(items.values())
-	for i in db.item.find({'_id':{'$in':items.keys()}}):
-		deltaT = latest - items[i['_id']]
-		timefactor = 1 / (1 + math.exp((deltaT.total_seconds()/24/3600.) - 60.))
-		pre['source'][i['source']] = pre['source'].get(i['source'], 0) + 1.5 * timefactor
-		pre['category'][i['category']] = pre['category'].get(i['category'], 0) + 1.5 * timefactor
-	
+	if len(favos) > 0:
+		latest = max(favos.values())
+		for i in db.item.find({'_id':{'$in':favos.keys()}}):
+			deltaT = latest - favos[i['_id']]
+			timefactor = 1 / (1 + math.exp((deltaT.total_seconds()/24/3600.) - 60.))
+			pre['source'][i['source']] = pre['source'].get(i['source'], 0) + 1.5 * timefactor
+			pre['category'][i['category']] = pre['category'].get(i['category'], 0) + 1.5 * timefactor
+			segs = filter(lambda s:s not in stopwords, jieba.cut(i['title'], cut_all=False))
+			for s in segs:
+				pre['wd'][s] = pre['wd'].get(s, 0) + 1 * timefactor
+	pre['wd'] = dict(sorted(pre['wd'].items(), key = lambda y:y[1], reverse=True)[:100])
 	db.upre.save(pre)
 	return pre
 		
@@ -76,7 +79,6 @@ def run():
 	for u in db.user.find(timeout=False):
 		userPre(u['_id'])
 	conn.close()
-
 
 if __name__ == '__main__':
 	global db
--- rsbackend/test.py
+++ rsbackend/test.py
@@ -1,26 +1,35 @@
 # -*- coding: utf-8 -*-
-import pymongo
-conn = pymongo.Connection('54.187.240.68')
-db = conn['feed']
-db.authenticate('JKiriS','910813gyb')
+# import pymongo
+# conn = pymongo.Connection('54.187.240.68')
+# db = conn['feed']
+# db.authenticate('JKiriS','910813gyb')
 
-upre = db.upre.find_one({'visited':{'$ne':[]}})
-vitemids = upre['visited'][-100:]
+# upre = db.upre.find_one({'visited':{'$ne':[]}})
+# vitemids = upre['visited'][-100:]
 
-texts_origin = []
-for i in db.item.find({'_id':{'$in':vitemids}}):
-    segs = filter(lambda s:s not in stopwords, jieba.cut(i['title'], cut_all=False))
-    segs *= 2
-    segs += filter(lambda s:s not in stopwords, jieba.cut(i['des'], cut_all=False))
-    texts_origin.append(segs)
+# texts_origin = []
+# for i in db.item.find({'_id':{'$in':vitemids}}):
+#     segs = filter(lambda s:s not in stopwords, jieba.cut(i['title'], cut_all=False))
+#     segs *= 2
+#     segs += filter(lambda s:s not in stopwords, jieba.cut(i['des'], cut_all=False))
+#     texts_origin.append(segs)
 
-from gensim import corpora, models, similarities
-lsi = models.LsiModel.load('G:/dav/rsbackend/gs.lsi')
-dic = corpora.Dictionary.load('G:/dav/rsbackend/gs.dic')
-index = similarities.MatrixSimilarity.load('G:/dav/rsbackend/gs.index')
-res = None
-for i in texts_origin:
-    test_bow = dic.doc2bow(i)
-    test_lsi = lsi[test_bow]
-    res = index[test_lsi] if res == None else res + index[test_lsi]
-res /= max(res)
+# from gensim import corpora, models, similarities
+# lsi = models.LsiModel.load('G:/dav/rsbackend/gs.lsi')
+# dic = corpora.Dictionary.load('G:/dav/rsbackend/gs.dic')
+# index = similarities.MatrixSimilarity.load('G:/dav/rsbackend/gs.index')
+# res = None
+# for i in texts_origin:
+#     test_bow = dic.doc2bow(i)
+#     test_lsi = lsi[test_bow]
+#     res = index[test_lsi] if res == None else res + index[test_lsi]
+# res /= max(res)
+import json
+
+cs = json.load(file('categories.json'))
+def aa(f, s):
+	print s + f['name']
+	if 'children' in f:
+		for c in f['children']:
+			aa(c, s + '\t')
+aa(cs, '')

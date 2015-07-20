81d8626fb882afc673e7256c5aa9d6541ba43914
unknown <jkzgyb@gmail.com>
2015-07-20 17:15:17 +0800

    change kmeans

--- thrift/rs_kmeans.py
+++ thrift/rs_kmeans.py
@@ -59,8 +59,6 @@ def updateRList(uid):
 
 	score = None
 	for i in range(K):
-		if i != max_label:
-			continue
 		center = list(enumerate(np.mean(data[label==i], axis=0)))
 		center_score = index[center]  * label_count.get(i) / len(label)
 		# score = center_score if score is None else score + center_score

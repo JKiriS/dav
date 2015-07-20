6488a7695db79f3d77c611ce4ea7ed5fc82a3b00
unknown <jkzgyb@gmail.com>
2015-07-20 17:14:23 +0800

    change kmeans

--- thrift/rs_kmeans.py
+++ thrift/rs_kmeans.py
@@ -64,10 +64,7 @@ def updateRList(uid):
 		center = list(enumerate(np.mean(data[label==i], axis=0)))
 		center_score = index[center]  * label_count.get(i) / len(label)
 		# score = center_score if score is None else score + center_score
-		if score is None:
-			score = center_score  
-		else:
-			np.vstack((score, center_score))
+		score = center_score if score is None else np.vstack((score, center_score))
 
 	print score.shape
 	score = np.amax(score, axis=0)
